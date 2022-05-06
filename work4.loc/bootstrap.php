046479a6ba7ae8c87a3d33891d64f36046ca43b5
Даниил <73055926+Fatlerne@users.noreply.github.com>
2022-05-06 14:00:04 +0300

    Написан скрипт инициализации в bootstrap.php

--- work4.loc/bootstrap.php
+++ work4.loc/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
-
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+require_once_DIR_.'/post.php';
+require_once_DIR_.'postservice.php';
+$postService = new PostService;
 
